18ce114ec36dba13435600d8a3f93c8239d51687
luandeptrainhat <115625569+luandeptrainhat@users.noreply.github.com>
2023-01-11 20:15:12 +0700

    asm

--- app/src/main/java/com/example/androi_nangcao/MainActivity.java
+++ app/src/main/java/com/example/androi_nangcao/MainActivity.java
@@ -13,27 +13,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //luân đẹp trai
-        Button btnStart = findViewById(R.id.btnStart);
-        Button btnStop = findViewById(R.id.btnStop);
-
-        btnStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, myService.class);
-                Bundle bundle = new Bundle();
-                bundle.putString("data", "haha");
-                bundle.putInt("tuoi",19);
-                intent.putExtras(bundle);
-                startService(intent);
-            }
-        });
-        btnStop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, myService.class);
-                stopService(intent);
-            }
-        });
     }
 }
